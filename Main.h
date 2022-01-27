1308d067a351414612fce073c8d247530651f247
ykb20128 <mohamad.alosh.2020@uni.strath.ac.uk>
2022-01-27 19:33:03 +0000

    test

---
+++ Main.h
@@ -0,0 +1,14 @@
+//
+// Created by ykb20128 on 27/01/2022.
+//
+
+#ifndef CS210_SIMPLE_AMOGUS_SHELL_MAIN_H
+#define CS210_SIMPLE_AMOGUS_SHELL_MAIN_H
+
+
+class Main {
+
+};
+
+
+#endif //CS210_SIMPLE_AMOGUS_SHELL_MAIN_H

f8e1fcd6241d71be2a09b94897ed106f7adccd0f
drb20147 <abel.sarandi.2020@uni.strath.ac.uk>
2022-02-06 22:10:45 +0000

    exiting works with ctrl-d and 'exit' added useful .gitignore

--- Main.c
+++ Main.c
@@ -5,44 +5,32 @@
 #include <stdbool.h>
 #include "Main.h"
 #include <string.h>
-int main (void) {
-    bool q = false;
-    int c = 0;
-    printf("%s\n","> Welcome to the Amogus Shell! Type exit or enter then CTRL+D to exit the shell");
-    while ((q == false) && (c != EOF)){
 
-
-        printf(">");
+int main(void) {
+    printf("%s\n", "> Welcome to the Amogus Shell! Type exit or CTRL+D to exit the shell");
+    while (1) {
+        printf("> ");
 
         char d_buffer[512];
 
-
-
-        gets(d_buffer);
-        if(d_buffer[0] == NULL){
-            printf("%s\n",">Press enter again if you want to continue or type CTRL+D to exit the shell");
-            c = fgetc(stdin);
-        }
-        /* if nothing was read from input, it then reads a new input waiting for CTRL+D, will put the value of
-           CTRL+D(which is EOF) in c and it will break the while loop if c == EOF*/
-
-
-        if (strcmp (d_buffer, "exit") == 0){
-            q = true;
+        if (fgets(d_buffer, 512, stdin) == NULL)
             break;
-        }
+        if (strcmp(d_buffer, "exit\n") == 0)
+            break;
 
         const char s[2] = " ";
         char *token;
         /* get the first token */
         token = strtok(d_buffer, s);
         /* walk through other tokens */
-        while( token != NULL ) {
-            printf( " %s%s\n", "<" ,token );
+        while (token != NULL) {
+            printf(" %s%s\n", "<", token);
 
             token = strtok(NULL, s);
         }
 
     }
+
+    printf("\nExiting...\n");
     return 0;
 }

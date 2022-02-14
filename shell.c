8bb0c299e16d1757827a5bb6340073ac26697cf6
Nam Nguyen Hai <nam.nguyen-hai.2020@uni.strath.ac.uk>
2022-02-14 20:24:06 +0100

    "execFork function added"

--- shell.c
+++ shell.c
@@ -5,27 +5,51 @@
 #include "shell.h"
 
 void run(){
-  printf("%s\n", "> Welcome to the Amogus Shell! Type exit or CTRL+D to exit the shell");
-  while (1) {
-      printf("> ");
+printf("%s\n", "> Welcome to the Amogus Shell! Type exit or CTRL+D to exit the shell");
+    while (1) {
+        char d_buffer[512];
+        char *inputArray[30];
+        int i = 0;
+        printf("> ");
 
-      char d_buffer[512];
-
-      if (fgets(d_buffer, 512, stdin) == NULL)
-          break;
-      if (strcmp(d_buffer, "exit\n") == 0)
-          break;
+        if (fgets(d_buffer, 512, stdin) == NULL)
+            break;
+        if (strcmp(d_buffer, "exit\n") == 0)
+            break;
 
       const char *s = " ;\t|><&;";
       char *token;
       /* get the first token */
-      token = strtok(d_buffer, s);
+        token = strtok(d_buffer, s);
       /* walk through other tokens */
-      while (token != NULL) {
-          printf(" %s%s\n", "<", token);
+        while (token != NULL) {
+            printf(" %s%s\n", "<", token);
+            inputArray[i] = token;
+            token = strtok(NULL, s);
+            i++;
+        }
+    }
+    printf("\nExiting...\n");
+}
 
-          token = strtok(NULL, s);
-      }
-  }
-  printf("\nExiting...\n");
+void execFork(char *inputArr[]){
+    int result;
+    int i = 0;
+
+    int pid = fork();
+
+    if(pid < 0){
+        printf("Fork was unsuccessful.\n");
+    } else if(pid == 0){
+        result = execvp(inputArr[0], inputArr);
+
+        if(result == -1){
+            char error[200];
+            snprintf(error, sizeof(error), "Erroe on %s", inputArr[0]);
+            perror(error);
+            exit(1);
+        }
+    } else {
+        wait(NULL);
+    }
 }

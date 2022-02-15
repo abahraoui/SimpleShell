a4b18a6074c16251c640e58de0ae57983a5ed8d1
gbb20158 <anis.bahraoui-jazouli.2020@uni.strath.ac.uk>
2022-02-15 00:28:10 +0000

    Stage 2

--- Main.c
+++ Main.c
@@ -2,32 +2,53 @@
 // Created by ykb20128 on 27/01/2022.
 //
 #include <stdio.h>
-#include <stdbool.h>
-#include "Main.h"
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+
 
 int main(void) {
+
     printf("%s\n", "> Welcome to the Amogus Shell! Type exit or CTRL+D to exit the shell");
+
     while (1) {
+        int i = 0;
         printf("> ");
 
-        char d_buffer[512];
+       const char *d_buffer[512];
 
         if (fgets(d_buffer, 512, stdin) == NULL)
             break;
         if (strcmp(d_buffer, "exit\n") == 0)
             break;
 
-        const char *s = " ;\t|><&;";
-        char *token;
+      char *commands[50]; // Stage 2 change, String array which store commands.
+
+
+
+        const char *s = " ;\t|><&\n;";
+        const char *token;
         /* get the first token */
         token = strtok(d_buffer, s);
         /* walk through other tokens */
         while (token != NULL) {
             printf(" %s%s\n", "<", token);
-
+            commands[i] = token;
             token = strtok(NULL, s);
+            i++;
         }
+
+         pid_t pid = fork();
+         if(pid < 0){
+             perror("fork failed");
+             exit(1);
+         }
+         else if(pid == 0){
+             execvp(commands[0], commands);
+         }
+         wait(NULL);
 
     }
 

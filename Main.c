0122c4611cfce6b08c88e8aca6805ae0ae2919a0
Anis Bahraoui Jazouli CS2020 <anis.bahraoui-jazouli.2020@uni.strath.ac.uk>
2022-02-15 20:37:17 +0000

    Update Main.c

--- Main.c
+++ Main.c
@@ -8,52 +8,61 @@
 #include <stdlib.h>
 
 
-int main(void) {
 
-    printf("%s\n", "> Welcome to the Amogus Shell! Type exit or CTRL+D to exit the shell");
+void execCommand(char * argv[]){
 
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        exit(1);
+    } else if (pid == 0) {
+        execvp(argv[0], argv);
+        perror("Error");
+        exit(1);
+    } else {
+        wait(NULL);
+    }
+}
+
+void run(void){
     while (1) {
-        int i = 0;
+
         printf("> ");
 
-        const char *d_buffer[512];
+        char *d_buffer[512];
 
         if (fgets(d_buffer, 512, stdin) == NULL)
             break;
         if (strcmp(d_buffer, "exit\n") == 0)
             break;
 
-        char *commands[50]; // Stage 2 change, String array which store commands.
-
-
-
-        const char *s = " ;\t|><&\n;";
+        char *commandArray[50]; // Stage 2 change, String array which store commands.
+        const char *delimiter = " ;\t|><&\n;";
         const char *token;
         /* get the first token */
-        token = strtok(d_buffer, s);
+        token = strtok(d_buffer, delimiter);
         /* walk through other tokens */
+        int i = 0;
         while (token != NULL) {
             //printf(" %s%s\n", "<", token);
-            commands[i] = token;
-            token = strtok(NULL, s);
+            commandArray[i] = token;
+            token = strtok(NULL, delimiter);
             i++;
         }
-        commands[i] = NULL;
+        commandArray[i] = NULL;
 
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork failed");
-            exit(1);
-        } else if (pid == 0) {
-            execvp(commands[0], commands);
-            perror("Error");
-            exit(1);
-        } else {
-            wait(NULL);
-        }
+        execCommand(commandArray);
 
     }
+}
 
+
+int main(void) {
+
+    printf("%s\n", "> Welcome to the Amogus Shell! Type exit or CTRL+D to exit the shell");
+    run();
     printf("\nExiting...\n");
     return 0;
 }
+

7f447f985eaf6c567a7ebd5b3f6a24a7b42079e0
gbb20158 <anis.bahraoui-jazouli.2020@uni.strath.ac.uk>
2022-03-04 00:06:36 +0000

    Stage 5 nearly done.

--- shell.c
+++ shell.c
@@ -71,15 +71,17 @@ void readInput(char *commandArray[]){
         execCommand(commandArray);
 }
 
+
 void run(void) {
     chdir(getenv("HOME"));
     char *currentPath[PATH_MAX];
-    char *history[20]; // created a history string array.
+    char *history[20]; /// created a history string array.
 
-    int historyCounter = 0; // created a counter for vacant entries to store commands.
+    int historyCounter = 0; /// created a counter for vacant entries to store commands.
     while (1) {
         getcwd(currentPath, sizeof(currentPath));
         printf("%s> ", currentPath);
+        printf(" %d ", historyCounter); ///printing the history counter to keep up.
 
         char *d_buffer[512];
 
@@ -105,55 +107,130 @@ void run(void) {
 
         commandArray[i] = NULL;
 
-        //trying to add all the input line into an entry of history.
+        ///trying to add all the input line into an entry of history.
         history[historyCounter] = malloc(sizeof(commandArray)*sizeof(commandArray[0]));
         int j = 0;
-        while( commandArray[j] != NULL){
-            if(strcspn(commandArray[j], "!") == 0) break;
-            if(strcspn(commandArray[j], "!") != 0)strcat(history[historyCounter], commandArray[j]);
-            if(commandArray[j+1] != NULL) strcat(history[historyCounter], " ");
-            j++;
+        if(strcspn(commandArray[j], "!") != 0){
+            while( commandArray[j] != NULL){
+                strcat(history[historyCounter], commandArray[j]);
+                if(commandArray[j+1] != NULL) strcat(history[historyCounter], " ");
+                j++;
+            }  ///incrementing the counter for next vacant entry.
+            /// keeping it circular by checking the counter vs size of array.
+            if(historyCounter == sizeof(history)/sizeof(history[0]) - 1) historyCounter = 0;
+            else historyCounter++;
         }
 
 
-        if(historyCounter == sizeof(history)/sizeof(history[0]) - 1) historyCounter = 0;
-       else historyCounter++; //incrementing the counter for next vacant entry.
+
 
 
 
         if (commandArray[0] == NULL) continue;
-        else if(strcmp(commandArray[0], "history") == 0){   // printing history
+        else if(strcmp(commandArray[0], "history") == 0){   /// printing history if history is called, can't do helper ??.
             int i = 0;
             while(history[i] != NULL){
-                printf("%s\n", *(history + i));
+                 printf("%d %s\n",i+1, *(history + i));
                 i++;
             }
 
+        }else if(strcmp(commandArray[0], "!!") == 0) { ///checking if user wants last command first,
+                                                      /// easier to look for ! or !-.
+            if(history[historyCounter-1] == NULL){   /// looking for if the invocation is valid and can be executed.
+                printf("%s\n","Invocation invalid, try again."); continue;
+            }
+            if(strcmp(history[historyCounter-1], "history") != 0){ /// checking if the last command
+                                  /// is history as its not in the execcommand (dont got helper funct)
+
+            token = strtok(history[historyCounter-1], " "); ///parsing the command in new array got problem
+            /// for multiple parameters commands.
+            char * commandInvoked[50];
+            int i = 0;
+            while(token != NULL){
+                commandInvoked[i] = token;
+                token = strtok(NULL, " ");
+                i++;
+            }
+            execCommand(commandInvoked); /// executing the command
+        } else {
+                int i = 0;     /// or printing the array if it's whats asked for
+                while(history[i] != NULL){
+                      printf("%d %s\n",i+1, *(history + i));
+                    i++;
+                }
+            }
         }
-        // here is the issue with !<no>
-        else if(strcspn(commandArray[0], "!") == 0){
+        else if(strcspn(commandArray[0], "-") == 1){ /// looking if its !-<no> command.
+
             int historyIndex;
-            // char charIndex = ? command array[0] = !2 -> 2
-            //  printf("%c", charIndex);
-           //   historyIndex = charIndex - '0';
-            printf("%d", historyIndex);
-            token = strtok(history[historyIndex], " ");
-           char * commandInvoked[50];
-           int i = 0;
-           while(token != NULL){
-               commandInvoked[i] = token;
-               token = strtok(NULL, " ");
-               i++;
-           } // post processing working just missing the right index.
-           int j = 0;
-           while(commandInvoked[j] != NULL){
-               printf("%s\n", *(history + j));
-               j++;
-           }
-            execCommand(commandInvoked);
+            const char *delimiter = "!-";
+            commandArray[0] = strtok(commandArray[0], delimiter); /// parsing to get the right index
+            historyIndex = atoi(commandArray[0]);
+            historyIndex = historyCounter - historyIndex;
+            /// looking for if the invocation is valid and can be executed.
+            if((history[historyIndex] == NULL)|| (historyIndex == historyCounter)){
+
+                printf("%s\n","Invocation invalid, try again."); continue;
+            }
+             /// looking for whether it's a valid index to invoke.
+            if( (historyIndex >= 0) && (historyIndex < 20)){
+                if(strcmp(history[historyIndex], "history") != 0){
+                    token = strtok(history[historyIndex], " ");
+                    char * commandInvoked[50];
+                    int i = 0;
+                    while(token != NULL){  ///usual parsing, dont like multiple parameter strings.
+                        commandInvoked[i] = token;
+
+                        token = strtok(NULL, " ");
+                        i++;
+                    }
+                    execCommand(commandInvoked);
+
+                }else {
+                    int i = 0; /// usual printing if it's asked for.
+                    while(history[i] != NULL){
+                          printf("%d %s\n",i+1, *(history + i));
+                        i++;
+                    }
+
+                }
+            }
+        }
+        else if(strcspn(commandArray[0], "!") == 0){ /// checking for !<no>
+            int historyIndex;
+            const char *delimiter = "!";   /// getting right index
+            commandArray[0] = strtok(commandArray[0], delimiter);
+
+            historyIndex =  atoi(commandArray[0]);
+            historyIndex -= 1;
+            /// checking if it's a right invoke.
+            if((history[historyIndex] == NULL) || (historyIndex == historyCounter)){
+                printf("%s\n","Invocation invalid, try again."); continue;
+            } /// checking if it's history.
+            if(strcmp(history[historyIndex], "history") != 0){
+                token = strtok(history[historyIndex], " ");
+                char * commandInvoked[50];
+
+                int i = 0;
+                while(token != NULL){
+                    commandInvoked[i] = token;
+
+                    token = strtok(NULL, " ");
+                    i++;
+                }
+
+                execCommand(commandInvoked);
+            }else {
+                int i = 0;
+                while(history[i] != NULL){
+                   printf("%d %s\n",i+1, *(history + i));
+                    i++;
+                }
+            }
         }
 
-        else readInput(commandArray);   //put all the reading input to choose command in a helper function.
+         /// sorry for all the else ifs lmao the code will be cleared when it's in helper functions.
+        else readInput(commandArray);   ///put all the reading input to choose command in a helper function.
 
     }
 
